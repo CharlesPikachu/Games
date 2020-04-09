1250a5d12b570f4b91f2447eab95b6fb6e5a13a0
CharlesPikachu <1159254961@qq.com>
2020-04-09 17:20:56 +0800

    add some explain in game3

--- Game3/cfg.py
+++ Game3/cfg.py
@@ -8,10 +8,12 @@ SCREENSIZE = (640, 640)
 PICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'resources/pictures')
 '''字体路径'''
 FONTPATH = os.path.join(os.getcwd(), 'resources/font/FZSTK.TTF')
-'''定义一些常量'''
+'''定义一些颜色'''
 BACKGROUNDCOLOR = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
+'''FPS'''
 FPS = 40
+'''随机打乱拼图次数'''
 NUMRANDOM = 100
