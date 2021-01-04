3d3dfc715063d757bde390e2ee7d0d4d43b3f20b
CharlesPikachu <1159254961@qq.com>
2021-01-04 14:20:19 +0800

    format the code styles from game1 to game10

--- Game2/cfg.py
+++ Game2/cfg.py
@@ -1,6 +1,7 @@
 '''配置文件'''
+import os
 
 
 '''图片路径'''
-BLOCK_IMAGE_PATH = 'resources/images/block.png'
-PIKACHU_IMAGE_PATH = 'resources/images/pikachu.png'
+BLOCK_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/images/block.png')
+PIKACHU_IMAGE_PATH = os.path.join(os.getcwd(), 'resources/images/pikachu.png')
